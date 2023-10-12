dd44526d9bd02bf80c6adc650e07f548f60008de
Bohdan Vlasenko <bodyavl228@gmail.com>
2023-10-12 20:01:06 +0300

    feat: lab 3

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,28 +7,35 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10;
     [SerializeField] float jumpSpeed = 10;
+    public ParticleSystem dust;
 
     Vector2 _moveInput;
     Rigidbody2D _myRigidBody;
     CapsuleCollider2D _myCaptuleCollider;
     Transform _myTransform;
+    Animator _myAnimator;
+
 
     // Start is called before the first frame update
     void Start()
     {
         _myRigidBody = GetComponent<Rigidbody2D>();
         _myCaptuleCollider = GetComponent<CapsuleCollider2D>();
+        _myTransform = GetComponent<Transform>();
+        _myAnimator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         Run();
+        FlipSprite();
     }
 
     void OnMove(InputValue value)
     {
         _moveInput = value.Get<Vector2>();
+
     }
 
     void OnJump(InputValue value)
@@ -42,7 +49,20 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
+
         Vector2 playerVelocity = new(_moveInput.x * runSpeed, _myRigidBody.velocity.y);
         _myRigidBody.velocity = playerVelocity;
+
+        bool hasHorizontalSpeed = Mathf.Abs(_myRigidBody.velocity.x) > Mathf.Epsilon;
+        _myAnimator.SetBool("isRunning", hasHorizontalSpeed);
+        if (hasHorizontalSpeed && _myCaptuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) dust.Play();
+        else dust.Stop();
+    }
+
+    void FlipSprite()
+    {
+        bool hasHorizontalSpeed = Mathf.Abs(_myRigidBody.velocity.x) > Mathf.Epsilon;
+        if (hasHorizontalSpeed) _myTransform.localScale = new Vector2(Mathf.Sign(_myRigidBody.velocity.x), 1f);
+
     }
 }
